c8b8af50bbe35c5c5112cd3477e3574b0a5bd005
Florin Bacu <bacuflorin@yahoo.com>
2018-03-21 15:04:45 +0200

    Call server telljoke

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -29,7 +29,7 @@ public class EndpointsAsyncTask  extends AsyncTask<Pair<Context, String>, Void, 
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("http://192.168.0.104:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
@@ -41,11 +41,9 @@ public class EndpointsAsyncTask  extends AsyncTask<Pair<Context, String>, Void, 
             myApiService = builder.build();
         }
 
-        context = params[0].first;
-        String name = params[0].second;
 
         try {
-            return myApiService.sayHi(name).execute().getData();
+            return myApiService.tellJoke().execute().getData();
         } catch (IOException e) {
             return e.getMessage();
         }
